78ab5b08d78551ec7d1bd0273ba6dad63aa2bd68
Rafael <167444291+babuinodev@users.noreply.github.com>
2024-05-15 20:00:45 -0300

    Update InterfaceUsuario.java

--- Aps/InterfaceUsuario.java
+++ Aps/InterfaceUsuario.java
@@ -1,4 +1,5 @@
 package Aps;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,7 +14,7 @@ public class InterfaceUsuario {
         int opcao;
         do {
             System.out.println("Menu:");
-            System.out.println("1. Pesquisar funcionário por ID");
+            System.out.println("1. Pesquisar funcionario por ID");
             System.out.println("2. Registrar venda de carro");
             System.out.println("3. Listar carros vendidos");
             System.out.println("4. Sair");
@@ -30,12 +31,12 @@ public class InterfaceUsuario {
                     System.out.print("Digite o ID do funcionário que vendeu o carro: ");
                     int idFuncionarioVenda = scanner.nextInt();
                     System.out.print("Digite o nome do carro vendido: ");
-                    scanner.nextLine();  // Consume newline left-over
+                    scanner.nextLine(); 
                     String nomeCarro = scanner.nextLine();
                     System.out.print("Digite o preço do carro vendido: ");
                     double precoCarro = scanner.nextDouble();
                     System.out.print("Digite o ano do carro vendido: ");
-                    int anoCarro = scanner.nextInt();
+                    int anoCarro = getAnoFromUser();
                     carrosVendidos.add(new Carro(idFuncionarioVenda, nomeCarro, nomeCarro, anoCarro, precoCarro));
                     AcessoDados.saveCarros(carrosVendidos, "file/carros.csv");
                     System.out.println("Venda registrada com sucesso.");
@@ -71,4 +72,35 @@ public class InterfaceUsuario {
             System.out.println("Funcionário com ID " + id + " não encontrado.");
         }
     }
+    public static int getAnoFromUser() {
+		System.out.println("Entre com o ano carro:");
+		try {
+			return getInterioPositivoFromUser();
+		}catch (InteiroPositivoFormatException e) {
+			System.out.println(e.getMessage());
+			System.out.println("Tente novamente");
+			return getAnoFromUser();
+		}
+		
+	}
+	
+	public static int getInterioPositivoFromUser() throws InteiroPositivoFormatException{
+		Scanner in = new Scanner(System.in);
+		String numeroString = in.nextLine();
+		try {
+			int numero = Integer.parseInt(numeroString);
+			if(numero<0) {
+				throw new InteiroPositivoFormatException("Numero " + numero + " eh negativo");
+			}
+			return numero;
+		}catch (NumberFormatException e) {
+			throw new InteiroPositivoFormatException("numero \"" + numeroString + "\" nao esta formatado como inteiro");
+		}
+	}
+	private static class InteiroPositivoFormatException extends IOException{
+		public InteiroPositivoFormatException(String message) {
+			super(message);
+		}}
+
 }
+
